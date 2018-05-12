ed70983eeb95601eb07635ecdf0730938523bd92
shafatahmad <shafat.ahmad@live.com>
2018-05-12 17:04:09 +0530

    Added index.html
    first commit

---
+++ js/portfolio.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  $("#portfolio-flters li").click (function(){
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+  
+    var selectedFilter = $(this).data("filter");
+    $("#portfolio-wrapper").fadeTo(100, 0);
+  
+    $(".portfolio-item").fadeOut().css('transform', 'scale(0)');
+  
+    setTimeout(function() {
+      $(selectedFilter).fadeIn(100).css('transform', 'scale(1)');
+      $("#portfolio-wrapper").fadeTo(300, 1);
+    }, 300);
+  });
+});
